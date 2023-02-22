3a4c12884a0b2773a84ff74b771175242bad807b
Elron <62204567+yslron@users.noreply.github.com>
2023-02-21 23:09:08 -0800

    Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# CHANGELOG.md
+
+## [1.0.1] - 2023-02-10
+
+- Minor changes
+- JS library updates
+
+
+## [1.0.0] - 2022-09-23
+
+First release
---
+++ js/main.js
@@ -0,0 +1,4 @@
+const stickyEls = document.querySelectorAll('[data-sticky]');
+if (stickyEls.length > 0) {
+  const sticky = new Sticky('[data-sticky]');
+}
